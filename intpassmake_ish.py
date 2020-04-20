2912e555daac91c0c81bd1802d157290e7fea37f
Ishihara-404 <ban.zai.gau@gmail.com>
2020-04-20 22:54:21 +0300

    Create intpassmake_ish.py

---
+++ intpassmake_ish.py
@@ -0,0 +1,398 @@
+import random
+import os
+os.system("pip install colorama")
+from colorama import *
+
+
+init()
+
+
+def col_s_13():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        r = random.randint(1, 9)
+        t = random.randint(1, 9)
+        y = random.randint(1, 9)
+        u = random.randint(1, 9)
+        i = random.randint(1, 9)
+        o = random.randint(1, 9)
+        p = random.randint(1, 9)
+        a = random.randint(1, 9)
+        s = random.randint(1, 9)
+        d = random.randint(1, 9)
+        
+        print(str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i) + str(o) + str(p) + str(a) + str(s) + str(d))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i) + str(o) + str(p) + str(a) + str(s) + str(d))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+
+
+def col_s_12():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        r = random.randint(1, 9)
+        t = random.randint(1, 9)
+        y = random.randint(1, 9)
+        u = random.randint(1, 9)
+        i = random.randint(1, 9)
+        o = random.randint(1, 9)
+        p = random.randint(1, 9)
+        a = random.randint(1, 9)
+        s = random.randint(1, 9)
+        
+        print(str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i) + str(o) + str(p) + str(a) + str(s))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i) + str(o) + str(p) + str(a) + str(s))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+
+def col_s_11():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        r = random.randint(1, 9)
+        t = random.randint(1, 9)
+        y = random.randint(1, 9)
+        u = random.randint(1, 9)
+        i = random.randint(1, 9)
+        o = random.randint(1, 9)
+        p = random.randint(1, 9)
+        a = random.randint(1, 9)
+        s = random.randint(1, 9)
+        
+        print(str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i) + str(o) + str(p) + str(a))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i) + str(o) + str(p) + str(a))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+
+def col_s_10():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        r = random.randint(1, 9)
+        t = random.randint(1, 9)
+        y = random.randint(1, 9)
+        u = random.randint(1, 9)
+        i = random.randint(1, 9)
+        o = random.randint(1, 9)
+        p = random.randint(1, 9)
+        print(str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i) + str(o) + str(p))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i) + str(o) + str(p))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+def col_s_9():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        r = random.randint(1, 9)
+        t = random.randint(1, 9)
+        y = random.randint(1, 9)
+        u = random.randint(1, 9)
+        i = random.randint(1, 9)
+        o = random.randint(1, 9)
+        print(str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i) + str(o))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i) + str(o))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+
+def col_s_8():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        r = random.randint(1, 9)
+        t = random.randint(1, 9)
+        y = random.randint(1, 9)
+        u = random.randint(1, 9)
+        i = random.randint(1, 9)
+        print(str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u) + str(i))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+
+def col_s_7():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        r = random.randint(1, 9)
+        t = random.randint(1, 9)
+        y = random.randint(1, 9)
+        u = random.randint(1, 9)
+        print(str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e) + str(r) + str(t) + str(y) + str(u))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+def col_s_6():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        r = random.randint(1, 9)
+        t = random.randint(1, 9)
+        y = random.randint(1, 9)
+        print(str(q) + str(w) + str(e) + str(r) + str(t) + str(y))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e) + str(r) + str(t) + str(y))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+
+def col_s_5():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        r = random.randint(1, 9)
+        t = random.randint(1, 9)
+        print(str(q) + str(w) + str(e) + str(r) + str(t))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e) + str(r) + str(t))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+
+def col_s_4():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        r = random.randint(1, 9)
+        print(str(q) + str(w) + str(e) + str(r))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e) + str(r))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+
+
+
+def col_s_3():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        e = random.randint(1, 9)
+        print(str(q) + str(w) + str(e))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w) + str(e))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+def col_s_2():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        w = random.randint(1, 9)
+        print(str(q) + str(w))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q) + str(w))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+
+def col_s_1():
+
+    f = open("password.txt", 'w')
+    for i in range(int(col_p)):
+
+        q = random.randint(1, 9)
+        print(str(q))
+        f = open("password.txt", 'a')
+        f.write("\n" + str(q))
+        f.close    
+
+    os.system("clear")
+    exit()
+
+
+
+
+
+
+os.system("clear")
+print( Fore.BLUE )
+print("|script by Ishihara|")
+print("")
+print( Fore.GREEN )
+print("<-----------Welcome----------->")
+print("")
+print("")
+print("")
+print("")
+print("Введите количество символов!") 
+print("От 1 до 13.")
+print("")
+col_s = input("[1-13]: ")
+os.system("clear")
+print( Fore.BLUE )
+print("|script by Ishihara|")
+print("")
+print( Fore.GREEN )
+print("<-----------Welcome----------->")
+print("")
+print("")
+print("")
+print("")
+print("Сколько паролей вы хотите")
+print("cгенерировать?")
+print("")
+col_p = input("[]: ")
+
+
+if col_s == "13":
+    col_s_13()
+
+elif col_s == "12":
+    col_s_12()
+
+elif col_s == "11":
+    col_s_11()
+
+elif col_s == "10":
+    col_s_10()
+
+elif col_s == "9":
+    col_s_9()
+
+elif col_s == "8":
+    col_s_8()
+
+elif col_s == "7":
+    col_s_7()
+
+elif col_s == "6":
+    col_s_6()
+
+elif col_s == "5":
+    col_s_5()
+
+elif col_s == "4":
+    col_s_4()
+
+elif col_s == "3":
+    col_s_3()
+
+elif col_s == "2":
+    col_s_2()
+
+elif col_s == "1":
+    col_s_1()
+
+
+else:
+    os.system("clear")
+    exit()
